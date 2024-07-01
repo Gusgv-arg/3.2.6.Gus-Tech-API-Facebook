bdbdc77a7a1503c100fb1136581988f90873115a
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2024-07-01 15:03:01 -0300

    log

--- controllers/postWhastsappWebhookController.js
+++ controllers/postWhastsappWebhookController.js
@@ -5,6 +5,24 @@ export const postWhatsappWebhookController = async (req, res) =>{
     const body = req.body;
 	console.log("Lo que recibo x WhatsApp de la API de facebook -->", body);
     console.log("body.entry[0].changes[0]:", body.entry[0].changes[0])
+    /* Object received
+    {
+        value:
+        {
+            messaging_product: 'whatsapp',
+            metadata:
+            {
+                display_phone_number: '15550480577', --> phone from where the msge is sent
+                phone_number_id: '312359751967984'
+            },
+            contacts: [ [Object] ],
+            messages: [ [Object] ]
+        },
+        field: 'messages'
+    } */
+
+   console.log("body.entry[0].changes[0].value.contacts[0]:", body.entry[0].changes[0].value.contacts[0])
+   console.log("body.entry[0].changes[0].value.messages[0]:", body.entry[0].changes[0].value.messages[0])
 
     res.status(200). send("Received")
 }
