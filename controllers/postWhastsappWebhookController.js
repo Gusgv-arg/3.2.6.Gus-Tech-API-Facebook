90dff627bde3ec2765cd932f49bcca79ca15ee11
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2024-07-01 14:29:46 -0300

    log

--- controllers/postWhastsappWebhookController.js
+++ controllers/postWhastsappWebhookController.js
@@ -4,6 +4,7 @@ export const postWhatsappWebhookController = async (req, res) =>{
 
     const body = req.body;
 	console.log("Lo que recibo x WhatsApp de la API de facebook -->", body);
+    console.log("body.entry[0].changes[0]:", body.entry[0].changes[0])
 
     res.status(200). send("Received")
 }
