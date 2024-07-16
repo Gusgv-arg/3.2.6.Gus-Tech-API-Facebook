842b3d00adb7aaa4f38447b21242204a29a82bf3
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2024-07-16 16:43:05 -0300

    log

--- middlewares/userMiddleware.js
+++ middlewares/userMiddleware.js
@@ -8,22 +8,22 @@ export const userMiddleware = async (req, res, next) => {
 	console.log("Lo que recibo de la API de facebook -->", body);
 
 	// WhatsApp
-	if (body.entry[0].changes[0].value.statuses) {
+	if (body?.entry[0]?.changes[0].value.statuses) {
 		console.log(
 			"WhatsApp Statuses--->",
 			body.entry[0].changes[0].value.statuses[0]
 		);
 		res.status(200).send("EVENT_RECEIVED");
-	} else if (body.entry[0].changes[0]) {
+	} else if (body?.entry[0]?.changes[0]) {
 		console.log("WhatsApp Changes -->", body.entry[0].changes[0]);
-	} else if (body.page && body.entry[0]) {
+	} else if (body?.page && body?.entry[0]) {
 		console.log("Messenger Messaging -->", body.entry[0].messaging[0]);
 	} else {
 		console.log("Other object");
 	}
 
 	// ------ WhatsApp -------//
-	if (body.entry[0]) {
+	if (body?.entry[0]) {
 		if (
 			body.entry &&
 			body.entry[0].changes &&
@@ -91,7 +91,7 @@ export const userMiddleware = async (req, res, next) => {
 		}
 
 		//-------- Messenger ----------//
-	} else if (body.object === "page") {
+	} else if (body?.object === "page") {
 		const senderId = body?.entry[0]?.messaging[0].sender.id;
 		const messengerMessage = body?.entry[0]?.messaging[0].message.text;
 		const name = "Messenger user";
