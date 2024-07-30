376149e3537f8352fa8a544789584b23c1bba80c
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2024-07-30 19:11:30 -0300

    bearer

--- utils/getAudioWhatsappUrl.js
+++ utils/getAudioWhatsappUrl.js
@@ -7,9 +7,13 @@ const whatsappToken = process.env.WHATSAPP_TOKEN;
 const myPhoneNumberId = process.env.WHATSAPP_PHONE_ID;
 
 export const getAudioWhatsappUrl = async (audioId) => {
-	const url = `https://graph.facebook.com/v20.0/${audioId}?phone_number_id=${myPhoneNumberId}?access_token=${whatsappToken}`;
+	const url = `https://graph.facebook.com/v20.0/${audioId}?phone_number_id=${myPhoneNumberId}`;
 	try {
-		const audioUrl = await axios.get(url);
+		const audioUrl = await axios.get(url, {
+			headers: {
+				'Authorization': `Bearer ${whatsappToken}`
+			}
+		});
 		console.log("Audio URL en getAudioWhatsappUrl", audioUrl);
 		return audioUrl;
 	} catch (error) {
