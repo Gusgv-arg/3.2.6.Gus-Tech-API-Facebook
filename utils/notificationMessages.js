78d9c5dc3d156aa9bb8b0468663559420d70bb7f
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2024-08-28 17:06:27 -0300

    detail message

--- utils/notificationMessages.js
+++ utils/notificationMessages.js
@@ -2,4 +2,4 @@ export const botSwitchOnNotification = "NOTIFICACION: MegaBot fue puesto en ON y
 
 export const botSwitchOffNotification = "NOTIFICACION: MegaBot fue puesto en OFF por lo que no responderá."
 
-export const helpFunctionNotification = "¡Bienvenido Administrador de MegaBot!\nA continuación te listo las funcionalidades con solo enviar un mensaje de texto.\n\nMegabot ayuda: envía este mensaje en donde se enumeran las funcionalidades.\nMegabot responder: encendido de MegaBot.\nMegaBot no responder: apagado de MegaBot.\n\n¡Con el tiempo iremos sumando más! "
+export const helpFunctionNotification = "¡Bienvenido Administrador de MegaBot!\nA continuación te listo las funcionalidades con solo enviar un mensaje de texto:\n\n1. Megabot ayuda: envía este mensaje en donde se enumeran las funcionalidades.\n2. Megabot responder: encendido de MegaBot.\n3. MegaBot no responder: apagado de MegaBot.\n\n¡Con el tiempo iremos sumando más! "
