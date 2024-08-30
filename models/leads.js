1bd1aebb65c91e15a4e99807436fdbb6f7cc0f2b
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2024-08-30 12:24:56 -0300

    lead model with campaign

--- models/leads.js
+++ models/leads.js
@@ -1,5 +1,20 @@
 import mongoose from "mongoose";
 
+const messageCampaignSchema = new mongoose.Schema({
+	messages: String,
+	status: { type: String, enum: ["contactado", "respuesta", "error"] },
+	sentAt: Date,
+	error: String,
+	retryCount: { type: Number, default: 0 },
+});
+
+const campaignDetailSchema = new mongoose.Schema({
+	campaignName: String,
+	campaignDate: Date,
+	campaignThreadId: String,
+	messages: [messageCampaignSchema],
+});
+
 const leadsSchema = new mongoose.Schema(
 	{
 		name: { type: String },
@@ -7,8 +22,9 @@ const leadsSchema = new mongoose.Schema(
 		channel: { type: String },
 		content: { type: String, required: true },
 		thread_id: { type: String },
-		botSwitch: { type: String, enum: ['ON', 'OFF'], required: true },	
-		responses: {type: Number, required: true}	
+		botSwitch: { type: String, enum: ["ON", "OFF"], required: true },
+		responses: { type: Number, required: true },
+		campaigns: [campaignDetailSchema],
 	},
 	{
 		timestamps: true,
--- utils/notificationMessages.js
+++ utils/notificationMessages.js
@@ -4,4 +4,4 @@ export const botSwitchOffNotification = "*NOTIFICACION:* MegaBot fue puesto en *
 
 export const helpFunctionNotification = `*¡Bienvenido Administrador de MegaBot!*\nA continuación se listan las funcionalidades al enviar los siguientes mensajes de texto por WhatsApp:\n\n1. *"Megabot":* envía este mensaje en donde se enumeran las funcionalidades disponibles.\n2. *"Megabot responder":* encendido de MegaBot.\n3. *"MegaBot no responder":* apagado de MegaBot.\n4. *"Campaña":* adjuntar Excel y en comentario colocar la palabra "campaña y nombre de la Plantilla de WhatsApp" *(Ej.: "campaña promocion1")*. El formato del Excel debe tener encabezados en donde la columna A debe ser el WhatsApp al cual enviar la plantilla de Campaña y el resto serán las variables ordenadas de acuerdo a la Plantilla.\n\n*¡Con el tiempo iremos sumando más!*`
 
-export const noPromotions = "*NOTIFICACION:* Tomamos conocimiento de que no desea recibir promociones. ¡Que tenga un buen día!"
+export const noPromotions = "*NOTIFICACION:* Tomamos conocimiento de que *NO* desea recibir más promociones. ¡Que tenga un buen día!"
