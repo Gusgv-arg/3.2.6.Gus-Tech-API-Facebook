9f2a3d3e75395a7c0d164cd28a660ebfac02715f
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2024-09-12 17:39:47 -0300

    campaña inactiva

--- utils/changeCampaignStatus.js
+++ utils/changeCampaignStatus.js
@@ -1,20 +1,34 @@
 import Leads from "../models/leads.js";
+import { adminWhatsAppNotification } from "./adminWhatsAppNotification.js";
 
 export const changeCampaignStatus = async (campaignStatus, campaignName) => {
 	try {
 		const users = await Leads.find();
+		let counter = 0;
+		let status = "";
 
 		for (const user of users) {
 			const campaign = user.campaigns.find(
 				(c) => c.campaignName === campaignName
 			);
+
 			if (campaign) {
-				campaign.campaign_status =
-					campaignStatus === "activar" ? "activa" : "inactiva"; // Cambiar el estado
+				if (campaignStatus === "activar") {
+					campaign.campaign_status = "activa";
+					status = "activa";
+					counter++;
+				} else if (campaignStatus === "inactivar") {
+					campaign.campaign_status = "inactiva";
+					status = "inactiva";
+					counter++;
+				}
 			}
 		}
 
 		await Promise.all(users.map((user) => user.save()));
+		await adminWhatsAppNotification(
+			`*NOTIFICACION cambio Status de Campaña:*\nPara la Campaña *${campaignName}* se cambiaron *${counter}* registros de clientes al status de Campaña *${status}*.`
+		);
 	} catch (error) {
 		console.log("Error en changecampaignStatus.js", error.message);
 		throw error;
