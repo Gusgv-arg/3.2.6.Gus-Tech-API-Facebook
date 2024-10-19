397262a8b9899b3b6fa34fbdc0d57be691c68621
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2024-10-19 20:21:22 -0300

    upsert

--- middlewares/userInstagramMiddleware.js
+++ middlewares/userInstagramMiddleware.js
@@ -11,18 +11,24 @@ dotenv.config();
 const maxResponses = process.env.MAX_RESPONSES;
 const ownerInstagramAccount = process.env.INSTAGRAM_OWNER_ACCOUNT_ID;
 
-// Middleware that creates the user in DB if it doesn't exist || next()
 export const userInstagramMiddleware = async (req, res, next) => {
 	const body = req.body;
-	//console.log("Lo que recibo de la API de Instagram -->", body);
 	let channel = body.object === "instagram" ? "instagram" : "other";
 	let senderId = body?.entry?.[0]?.messaging?.[0]?.sender?.id || "";
 	let recipientId = body?.entry?.[0]?.messaging?.[0]?.recipient?.id || "";
 
-	if (channel === "instagram" && body?.entry[0]?.messaging) {
-		// console.log("body.entry[0].messaging[0] -->", body.entry[0]?.messaging[0] ?? "No messaging data");
-		// console.log("Attachments -->", body?.entry[0]?.messaging[0]?.message?.attachments?.[0] ? body.entry[0].messaging[0].message.attachments[0] : "no attachments");
+	if (channel !== "instagram" || senderId === "") {
+		console.log("Object not processed by API:", body);
+		return res.status(200).send("EVENT_RECEIVED");
+	}
 
+	// Check if its a message for my account or is a meesage sent by me to my propoer account
+	if (senderId === ownerInstagramAccount || recipientId !== ownerInstagramAccount) {
+		console.log("Return because of owner account or different recipient ID");
+		return res.status(200).send("EVENT_RECEIVED");
+	}
+
+	if (channel === "instagram" && body?.entry[0]?.messaging) {
 		const type = body?.entry[0]?.messaging[0]?.message?.attachments?.[0]?.type
 			? body.entry[0].messaging[0].message.attachments[0].type
 			: "text";
@@ -34,94 +40,69 @@ export const userInstagramMiddleware = async (req, res, next) => {
 			: senderId;
 	}
 
-	if ((channel === "instagram") & (senderId !== "")) {
-		// Check if iths the owner of the account and return
-		if (senderId === ownerInstagramAccount) {
-			console.log("Return because is the owner of the account!!");
-			res.status(200).send("EVENT_RECEIVED");
-			return;
-		} else if (recipientId !== ownerInstagramAccount) {
-			// Check if its another recipient ID different from our Instagram ID and return
-			console.log(
-				"Return because the recipient ID is not owrs!! RecipientId:",
-				recipientId
-			);
-			res.status(200).send("EVENT_RECEIVED");
-			return;
-		}
+	const instagramMessage =
+		body?.entry?.[0]?.messaging?.[0]?.message?.text ??
+		body?.entry?.[0]?.standby?.[0]?.message?.text ??
+		"";
+	const name = "Instagram user";
+	const instagramMessageId = body?.entry?.[0]?.messaging?.[0]?.message?.mid
+		? body.entry[0].messaging[0].message.mid
+		: "";
 
-		const instagramMessage =
-			body?.entry?.[0]?.messaging?.[0]?.message?.text ??
-			body?.entry?.[0]?.standby?.[0]?.message?.text ??
-			"";
-		const name = "Instagram user";
-		const instagramMessageId = body?.entry?.[0]?.messaging?.[0]?.message?.mid
-			? body.entry[0].messaging[0].message.mid
-			: "";
+	const currentDateTime = new Date().toLocaleString("es-AR", {
+		timeZone: "America/Argentina/Buenos_Aires",
+		day: "2-digit",
+		month: "2-digit",
+		year: "numeric",
+		hour: "2-digit",
+		minute: "2-digit",
+		second: "2-digit",
+	});
 
-		// Find the lead by id
-		let lead = await Leads.findOne({ id_user: senderId });
+	try {
+		// Function that creates a lead or if it exists it updates it
+		const lead = await Leads.findOneAndUpdate(
+			{ id_user: senderId },
+			{
+				$setOnInsert: {
+					name: name,
+					content: `${currentDateTime} - ${name}: ${instagramMessage}\n${currentDateTime} - MegaBot: ${messengerGreeting}`,
+					botSwitch: "ON",
+					channel: channel,
+					responses: 1,
+				},
+				$addToSet: { instagramMid: instagramMessageId },
+				$inc: { responses: 1 },
+			},
+			{ new: true, upsert: true }
+		);
 
-		if (lead === null) {
-			// Obtain current date and hour
-			const currentDateTime = new Date().toLocaleString("es-AR", {
-				timeZone: "America/Argentina/Buenos_Aires",
-				day: "2-digit",
-				month: "2-digit",
-				year: "numeric",
-				hour: "2-digit",
-				minute: "2-digit",
-				second: "2-digit",
-			});
-
-			lead = await Leads.create({
-				name: name,
-				id_user: senderId,
-				content: `${currentDateTime} - ${name}: ${instagramMessage}\n${currentDateTime} - MegaBot: ${messengerGreeting}`,
-				instagramMid: [instagramMessageId],
-				botSwitch: "ON",
-				channel: channel,
-				responses: 1,
-			});
+		if (lead.instagramMid.length === 1) {
+			// Es un nuevo lead
 			console.log("Lead created in Leads DB!!:", name);
-
-			// Post greeting to the new customer
 			await handleMessengerGreeting(senderId);
-
-			// Create a Thread sending user message and greeting to GPT
 			const thread = await createGptThread(name, instagramMessage, channel);
-
-			// Save thread in DB
 			lead.thread_id = thread;
 			await lead.save();
 			console.log("Lead updated with threadId!!");
-
-			// Send Notification of new lead to Admin
 			newLeadWhatsAppNotification(channel, name);
 			console.log("Lead creation notification sent to Admin!!");
-
-			res.status(200).send("EVENT_RECEIVED");
 		} else {
-			
-			// Check max allowed responses
-			if (
-				lead.responses + 1 > maxResponses &&
-				senderId !== "1349568352682541"
-			) {
+			// Es un lead existente
+			if (lead.responses > maxResponses && senderId !== "1349568352682541") {
 				console.log("User reached max allowed responses");
 				await handleMessengerMaxResponses(senderId);
 				return res.status(200).send("EVENT_RECEIVED");
 			}
 
-			// Check individual botSwitch
 			if (lead.botSwitch === "OFF") {
 				return res.status(200).send("EVENT_RECEIVED");
-			}			
+			}
 		}
 
 		next();
-	} else {
-		console.log("Object not processed by API:", body);
-		res.status(200).send("EVENT_RECEIVED");
+	} catch (error) {
+		console.error("Error in userInstagramMiddleware:", error);
+		res.status(500).send("Internal Server Error");
 	}
 };
