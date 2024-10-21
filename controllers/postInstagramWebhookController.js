15507ea4600256579aad7e96feaaf7bc4d3a2ecb
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2024-10-21 16:03:41 -0300

    for  instead of while

--- controllers/postInstagramWebhookController.js
+++ controllers/postInstagramWebhookController.js
@@ -11,52 +11,53 @@ export const postInstagramWebhookController = (req, res) => {
 	const body = req.body;
 	console.log("Recibo en postInstagramWebhookController.js: body-->", body);
 
+	if (body.object !== "instagram") {
+		return res.sendStatus(404);
+	}
+
+	let shouldRespond = true;
+
 	// Check if its Instagram App
-	if (body.object === "instagram") {
-		// Facebook sends an array that can have more than 1
-		body.entry.forEach(async (entry) => {
-			// Gets body of the webhook event
-			let webhook_event = entry?.messaging?.[0] ?? entry?.standby?.[0] ?? null;
-			console.log("webhook_event-->", webhook_event);
-			
-			// Get the sender ID
-			let senderId = webhook_event.sender?.id ?? "";
-			//console.log("senderId en postInstagramWebhookController.js:", senderId);
+	for (const entry of body.entry) {
+		// Gets body of the webhook event
+		let webhook_event = entry?.messaging?.[0] ?? entry?.standby?.[0] ?? null;
+		console.log("webhook_event-->", webhook_event);
 
-			// Get the recipient ID
-			const recipientId = webhook_event.recipient?.id ?? "";
-			if (
-				senderId === ownerInstagramAccount ||
-				recipientId !== ownerInstagramAccount
-			) {
-				console.log("Return because of owner account or different recipient ID");
-				return res.status(200).send("EVENT_RECEIVED");
-			}
+		// Get the sender ID
+		let senderId = webhook_event.sender?.id ?? "";
+		//console.log("senderId en postInstagramWebhookController.js:", senderId);
 
-			if (webhook_event.message) {
-				const channel = "instagram";
-				const name = "Instagram user";
-				// Get the message sent by the user & create an object to send it to the queue
-				const userMessage = {
-					channel: channel,
-					message: webhook_event.message.text ? webhook_event.message.text : "",
-					name: name,
-					type: req.type ? req.type : "text",
-					url: body?.entry[0]?.messaging?.[0]?.message?.attachments?.[0]
-						?.payload.url
-						? body.entry[0].messaging[0].message.attachments[0].payload.url
-						: "",
-				};
+		// Get the recipient ID
+		const recipientId = webhook_event.recipient?.id ?? "";
 
-				// Add message to the Instagram Queue
-				messageQueue.enqueueInstagramMessage(userMessage, senderId);
-			}
-		});
+		if (
+			senderId === ownerInstagramAccount ||
+			recipientId !== ownerInstagramAccount
+		) {
+			console.log("Skipping message due to sender or recipient mismatch");
+			continue;
+		}
 
-		// Returns a '200 OK' response to all requests
+		if (webhook_event.message) {
+			const channel = "instagram";
+			const name = "Instagram user";
+			// Get the message sent by the user & create an object to send it to the queue
+			const userMessage = {
+				channel: channel,
+				message: webhook_event.message.text ? webhook_event.message.text : "",
+				name: name,
+				type: req.type ? req.type : "text",
+				url: body?.entry[0]?.messaging?.[0]?.message?.attachments?.[0]?.payload
+					.url
+					? body.entry[0].messaging[0].message.attachments[0].payload.url
+					: "",
+			};
+
+			// Add message to the Instagram Queue
+			messageQueue.enqueueInstagramMessage(userMessage, senderId);
+		}
+	}
+	if (shouldRespond) {
 		res.status(200).send("EVENT_RECEIVED");
-	} else {
-		// Return a '404 Not Found' if event is not from a page subscription
-		res.sendStatus(404);
 	}
 };
