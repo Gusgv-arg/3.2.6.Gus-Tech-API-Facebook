2ddfb83db26f0f0f257de1324e669d0fbeb84260
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2025-01-10 11:32:14 -0300

    logs

--- flows/saveVendorFlow_2Response.js
+++ flows/saveVendorFlow_2Response.js
@@ -20,28 +20,38 @@ export const saveVendorFlow_2Response = async (
 
 	try {
 		// Looks existent with flowToken
-		let lead = await Leads.findOne({"flows.flow_token": flowToken});
-		
-		// Update existing lead
-		if (notification.includes("Respuesta del Vendedor: Atender")) {
-			lead.flows[0].client_status = "vendedor";
-			lead.flows[0].vendor_phone = senderId;
-			lead.flows[0].history += `${currentDateTime} - Status Cliente: Vendedor `;
-			console.log(`El vendedor ${senderId} aceptó atender al cliente ${lead.name}`)
+		let lead = await Leads.findOne({ "flows.flow_token": flowToken });
+		console.log("flow token:", flowToken)
+		console.log("lead en saveVendorFlow_2Response.js", lead)
 
-		} else if (notification.includes("Respuesta del Vendedor: No Atender")) {
-			lead.flows[0].client_status = "respuesta";
-			lead.flows[0].vendor_phone = null;
-			lead.flows[0].history += `${currentDateTime} - Status Cliente: Respuesta`;
-			console.log(`El vendedor ${senderId} NO aceptó atender al cliente ${lead.name}!!`)
-		
+		if (lead) {
+			// Update existing lead
+			if (notification.includes("Respuesta del Vendedor: Atender")) {
+				lead.flows[0].client_status = "vendedor";
+				lead.flows[0].vendor_phone = senderId;
+				lead.flows[0].history += `${currentDateTime} - Status Cliente: Vendedor `;
+				console.log(
+					`El vendedor ${senderId} aceptó atender al cliente ${lead.name}`
+				);
+			} else if (notification.includes("Respuesta del Vendedor: No Atender")) {
+				lead.flows[0].client_status = "respuesta";
+				lead.flows[0].vendor_phone = null;
+				lead.flows[0].history += `${currentDateTime} - Status Cliente: Respuesta`;
+				console.log(
+					`El vendedor ${senderId} NO aceptó atender al cliente ${lead.name}!!`
+				);
+			} else {
+				console.log(
+					"NO SE ESTA GRABANDO NADA en saveVendorFlow_2Response.js!!"
+				);
+			}
 		} else {
 			console.log("NO SE ESTA GRABANDO NADA en saveVendorFlow_2Response.js!!");
 		}
 
 		await lead.save();
 
-		return lead.name
+		return lead.name;
 	} catch (error) {
 		console.error(
 			"Error in saveVendorFlow_2Response.js:",
