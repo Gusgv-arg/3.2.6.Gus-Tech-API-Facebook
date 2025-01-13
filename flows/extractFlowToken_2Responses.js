d46afe0695a326e6ec829c0a0643c3db58b00871
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2025-01-13 18:08:58 -0300

    flow2

--- flows/extractFlowToken_2Responses.js
+++ flows/extractFlowToken_2Responses.js
@@ -2,19 +2,25 @@ export const extractFlowToken_2Responses = (userMessage) => {
 	let extraction = "";
 
 	// Extraer la respuesta del vendedor
-	const regex = /"Tomar Lead":"([^"]+)"/;
-	const atención = userMessage.match(regex);
-	extraction += `Respuesta del Vendedor: ${atención[1]}\n`;
+	if (userMessage.includes("Tomar Lead")){
+		const regex = /"Tomar Lead":"([^"]+)"/;
+		const atención = userMessage.match(regex);
+		extraction += `Respuesta del Vendedor: ${atención[1]}\n`;
+	}
 	
 	// Derivación del vendedor
-	const regex0 = /"Derivar Lead":"([^"]+)"/;
-	const derivar = userMessage.match(regex0);
-	extraction += `Derivación a Vendedor: ${derivar[1]}\n`;
+	if (userMessage.includes("Derivar Lead")){
+		const regex0 = /"Derivar Lead":"([^"]+)"/;
+		const derivar = userMessage.match(regex0);
+		extraction += `Derivación a Vendedor: ${derivar[1]}\n`;
+	}
 
-	// Extraer notas del vendedor
-	const regex1 = /"Notas sobre el lead":"([^"]+)"/;
-	const notas = userMessage.match(regex1);
-	extraction += `Notas del Vendedor: ${notas[1]}\n`;
+	// Extraer notas del vendedor.
+	if (userMessage.includes("Notas")){
+		const regex1 = /"Notas sobre el lead":"([^"]+)"/;
+		const notas = userMessage.match(regex1);
+		extraction += `Notas del Vendedor: ${notas[1]}\n`;
+	}
 
 	// Extraer token
 	const regex2 = /"flow_token":"([^"]+)"/;
@@ -23,4 +29,4 @@ export const extractFlowToken_2Responses = (userMessage) => {
 	return { extraction, flowToken };
 };
 
-/* extractFlowToken_2Responses('{"Tomar Lead":"Atender", "Derivar Lead":"Vendedor 1", "Notas sobre el lead":"Hola","flow_token":"2c2fd2ec9-3723-46c7-985f-332e11bc1fd4"}'); */
+extractFlowToken_2Responses('{"Tomar Lead":"Atender más tarde","flow_token":"2e57cdb81-8fd6-4e4d-a1cc-4bd89c673a38"}'); 
